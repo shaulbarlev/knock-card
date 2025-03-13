83894700de2f52509fce9c10857bcc606b5a5dda
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-03-13 13:34:12 +0200

    svg icons

--- knock-card-frontend/src/main.js
+++ knock-card-frontend/src/main.js
@@ -1,9 +1,19 @@
 import { gsap } from "gsap";
+import { CustomEase } from "gsap/CustomEase";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // gsap.fromTo(
+  //   ".overlay-container",
+  //   { rotationY: -75 },
+  //   {
+  //     rotationY: 0,
+  //     duration: 2.75,
+  //     ease: CustomEase.create("custom", "M0,0 C0.238,0.316 0.469,1 1,1 "),
+  //   }
+  // );
   gsap.fromTo(
     ".overlay-container",
-    { rotationY: -70 },
+    { rotationY: -100 },
     { rotationY: 0, duration: 3, ease: "power1.out" }
   );
 
@@ -16,6 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
   gsap.fromTo(
     ".overlay-container",
     { opacity: 0 },
-    { opacity: 1, duration: 2.5, ease: "power2.inOut" }
+    { opacity: 1, duration: 2.5, ease: "power2.inOut", delay: 0.35 }
   );
 });
